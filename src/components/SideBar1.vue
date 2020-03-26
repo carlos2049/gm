637532cfd8cbe214e284ba4c17d9f92174aa0ed0
carlos <carlos.20499@gmail.com>
2020-03-26 20:40:51 -0300

    comenzando el trabajo de navbar

---
+++ src/components/SideBar1.vue
@@ -0,0 +1,42 @@
+<template>
+<v-container>
+    <nav>
+        <v-app-bar app  color="teal lighten-3">
+     
+            <v-app-bar-nav-icon @click="miniVariant = !miniVariant"></v-app-bar-nav-icon>
+            
+
+        </v-app-bar>
+
+
+       
+        
+    </nav>  
+    <!-- color="blue-grey darken-4" -->
+     <v-navigation-drawer app   v-model="drawer" :mini-variant="miniVariant">
+            <div class="d-flex flex-row mb-7">
+                <h2> {{ nombre }} </h2>
+            </div>
+            
+            <v-divider></v-divider>
+           
+        </v-navigation-drawer>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'SideBar',
+    data(){
+        return{
+            drawer:true,
+             miniVariant: false,
+             nombre: 'ALTUS COACH'
+        }
+    }
+    
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/views/Prueba.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="prueba">
+   
+    <SideBar></SideBar>
+
+</div>
+  
+</template>
+
+<script>
+import SideBar from '@/components/SideBar1.vue'
+export default {
+    name: 'Prueba',
+    components: {
+        SideBar
+    }
+
+}
+</script>
+
+<style>
+
+</style>
